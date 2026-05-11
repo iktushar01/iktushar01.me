38036fb14da8a380ba79ede3b72f7f7e48407d26
iktushar01 <iktushar2020@gmail.com>
2026-05-11 07:34:53 +0600

    feat: implement education section and integrate it into homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/modules/homepage/navbar";
 import Header from "@/components/modules/homepage/header";
 import AboutMe from "@/components/modules/homepage/aboutme";
 import Skills from "@/components/modules/homepage/skills";
+import Education from "@/components/modules/homepage/education";
 export default function Home() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <AboutMe />
       <Skills />
+      <Education />
     </div>
   );
 }
